64ace59b25853f1acdf8df6ddd382d338283a6ca
André Huang Hong <73005945+andrehuang11@users.noreply.github.com>
2022-03-22 16:01:41 -0300

    Update quiz_simples.py

--- quiz_simples.py
+++ quiz_simples.py
@@ -3,27 +3,6 @@ pontuação = 0
 #contagem do número de vezes que a função "perguntas" foi chamada
 contagem = 0
 
-def perguntas(pergunta, resposta):
-    #soma 1 na contagem cada vez que essa função é chamada na main
-    global contagem
-    contagem += 1
-    #loop que continua até o usuário responder um número
-    while True:
-        try:
-            RESPOSTA = int(input(pergunta))
-        except ValueError:
-            print("DIGITE UM NÚMERO INTEIRO!")
-            continue
-        else:
-            break
-    if RESPOSTA != resposta:
-        print(f"Errou! A resposta correta é: {resposta}")
-    else:
-        print("Acertou!")
-        #soma 1 na contagem da pontuação cada vez que o usuário acerta a resposta
-        global pontuação
-        pontuação += 1
-
 def main():
     print("Bem vindo")
     #lista de todas as respostas possíveis
@@ -55,6 +34,27 @@ def main():
     #se o número de respostas certas for diferente do número de perguntas, o programa vai dizer ao usuário quantas perguntas ele acertou
     else:
         print(f"vc acertou um total de {pontuação} questão(ões)")
+        
+def perguntas(pergunta, resposta):
+    #soma 1 na contagem cada vez que essa função é chamada na main
+    global contagem
+    contagem += 1
+    #loop que continua até o usuário responder um número
+    while True:
+        try:
+            RESPOSTA = int(input(pergunta))
+        except ValueError:
+            print("DIGITE UM NÚMERO INTEIRO!")
+            continue
+        else:
+            break
+    if RESPOSTA != resposta:
+        print(f"Errou! A resposta correta é: {resposta}")
+    else:
+        print("Acertou!")
+        #soma 1 na contagem da pontuação cada vez que o usuário acerta a resposta
+        global pontuação
+        pontuação += 1
 
 main()
 #código para o cmd do windows não fechar instantaneamente após o fim do programa
